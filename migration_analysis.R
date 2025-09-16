9d1686f01faa141e3f39fc80ce6a117484dc84c7
Anna Palmer <anna.palmer@burnet.edu.au>
2025-09-15 17:42:56 -0700

    Update migration_analysis.R

--- migration_analysis.R
+++ migration_analysis.R
@@ -11,13 +11,109 @@ dat_all <- dat_all %>% filter(v005_denorm!=0)
 dat_all$country_year <- paste(dat_all$dhs_cde,  dat_all$year, sep = "_")
 
 
-# Plot the migration variable ---------------------------------------------
+# Create migration variables ---------------------------------------------
 
 # Take first observation for each person
 dat_first <- dat_all %>%
   group_by(caseid) %>%
   slice(1) %>%
   ungroup()
+
+# Year & age migrated
+dat_all$migration_year <- dat_all$v007-dat_all$v104
+dat_all$migration_age <- pmax(dat_all$v012-dat_all$v104, 0)
+
+# Variable for timing of migration
+dat_all <- dat_all %>%
+  mutate(
+    migration_timing = case_when(
+      # 1) Always lived in same place
+      al_lived == 1 ~ "Never migrated",
+      
+      # 2) Move occurred before age 10
+      !is.na(migration_age) & migration_age<10 ~ "Migrated before age 10",
+
+      # 3) Moved after
+      al_lived == 0 ~ "Migrated after age 10",
+      
+      # Unknown
+      TRUE ~ NA
+    )
+  )
+
+# TODO: How many migrated within the same region
+
+
+# Determine if people exposed to TCs migrate more -------------------------
+
+prop.table(table(dat_all$exp34, dat_all$migration_timing), margin=1)
+
+# TODO: Model migration as outcome
+
+# Analysis repeated among those who haven't migrated ----------------------
+
+# Subset the data
+dat_all_subs <- dat_all %>%
+  filter(migration_timing %in% c("Never migrated", "Migrated before age 10"))
+
+# Run the model
+mod <- feols(
+  married ~ 
+    i(dhs_cde, exp34) +
+    i(dhs_cde, exp34_lag1) +
+    i(dhs_cde, exp34_lag2) +
+    i(dhs_cde, exp34_lag3) +
+    age |
+    clustid + country_year,
+  data   = dat_all_subs,
+  vcov   = ~ clustid,
+  weights = ~ v005_denorm
+)
+
+# Tidy the results
+res_clean <- broom::tidy(mod, conf.int = TRUE) %>%
+  mutate(
+    dhs_cde   = str_extract(term, "(?<=::)[A-Z]{2}"),
+    variable  = str_remove(term, ".*:"),
+    country = countrycode(dhs_cde, "dhs", "country.name"),
+    year = case_when(
+      variable == "exp34"       ~ 0L,
+      variable == "exp34_lag1"  ~ 1L,
+      variable == "exp34_lag2"  ~ 2L,
+      variable == "exp34_lag3"  ~ 3L,
+      TRUE ~ NA_integer_)
+  ) %>%
+  select(term, estimate, conf.low, conf.high, std.error, statistic, p.value, country, year) %>%
+  filter(!is.na(country))
+
+# Plot the results
+plot <- ggplot(res_clean, aes(x = year, y = estimate, group = country)) +
+  geom_line(linewidth = 0.5) +
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2, color = NA) +
+  geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.25) +
+  labs(
+    x = "Years since tropical cyclone",
+    y = "Change in the annual rate of child marriage"
+  ) +
+  theme_minimal() +
+  theme(
+    panel.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text = element_text(),
+    axis.ticks.length = grid::unit(3, "pt"),
+    axis.ticks = element_line(linewidth = 0.25),
+    plot.title = element_text(face = "bold"),
+    panel.border = element_rect(fill = NA, linewidth = 0.5),
+    strip.text = element_text(hjust = 0)
+  ) +
+  facet_wrap(~ country, ncol = 4) +
+  coord_cartesian(ylim = c(-0.05, 0.05), xlim = c(-0.5, 3.5))
+ggsave("figures/main_migration.jpeg", plot, height = 6, width = 6)
+
+
+
+
 
 # Calculate variable for misclassified
 dat_first <- dat_first %>%
